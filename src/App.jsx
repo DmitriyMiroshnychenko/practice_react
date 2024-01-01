c577f3b1b40f3fa3f94a95a87b3771bbc5aea033
Dmytro <88747340+DmitriyMiroshnychenko@users.noreply.github.com>
2024-01-01 22:17:00 +0200

    added clicker

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import React, { Component } from "react";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0,
-      someKey: false,
-    };
-  }
-  handleClick = () => {
+  state = {
+    count: 0,
+  };
+
+  increment = () => {
     this.setState({ count: this.state.count + 1 });
+  };
+
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
   };
 
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{ margin: "auto", width: "300px" }}>
         Hello from React
-        <button onClick={this.handleClick}>{this.state.count}</button>
+        <button onClick={this.decrement}>-</button>
+        <span style={{ margin: "0 0.5rem", display: "inline-block" }}>
+          {this.state.count}
+        </span>
+        <button onClick={this.increment}>+</button>
       </div>
     );
   }
