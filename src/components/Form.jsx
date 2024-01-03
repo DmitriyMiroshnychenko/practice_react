d4d2299f2ecec6ca682e607c24d371b6664b834a
Dmytro <88747340+DmitriyMiroshnychenko@users.noreply.github.com>
2024-01-03 22:53:19 +0200

    update file

---
+++ src/components/Form.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+class Form extends React.Component {
+  state = {
+    firstName: "",
+  };
+  handleChange = (event) => {
+    this.setState({ firstName: event.target.value });
+  };
+
+  render() {
+    const { firstName } = this.state;
+    return (
+      <div>
+        <input
+          type="text"
+          name="firstName"
+          placeholder="firstName"
+          value={firstName}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
+}
+
+export { Form };
